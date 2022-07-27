cd0a2ab01b2215f5346c00374c29fbbecc144884
Viorica Mancas <viorica.mancas@crowdstrike.com>
2022-07-27 15:03:50 +0300

    add lab3 demo and slides

---
+++ lab03/demo/select/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
+}
